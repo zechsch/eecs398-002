988f890462b9a4df4183848f343ec0d74c18da36
Zechariah Schneider <zechsch@umich.edu>
2016-03-22 20:08:32 -0400

    idkman

--- kineval/kineval_inverse_kinematics.js
+++ kineval/kineval_inverse_kinematics.js
@@ -37,7 +37,29 @@ kineval.randomizeIKtrial = function randomIKtrial () {
     kineval.params.trial_ik_random.time = cur_time.getTime()-kineval.params.trial_ik_random.start.getTime();
 
     // STENCIL: see instructor for random time trial code
+    // update time from start of trial
+  cur_time = new Date();
+  kineval.params.trial_ik_random.time = cur_time.getTime()-kineval.params.trial_ik_random.start.getTime();
+
+  // get endeffector Cartesian position in the world
+  endeffector_world = matrix_multiply(robot.joints[robot.endeffector.frame].xform,robot.endeffector.position);
+
+  // compute distance of endeffector to target
+  kineval.params.trial_ik_random.distance_current = Math.sqrt(
+          Math.pow(kineval.params.ik_target.position[0][0]-endeffector_world[0][0],2.0)
+          + Math.pow(kineval.params.ik_target.position[1][0]-endeffector_world[1][0],2.0)
+          + Math.pow(kineval.params.ik_target.position[2][0]-endeffector_world[2][0],2.0) );
+
+  // if target reached, increment scoring and generate new target location
+  if (kineval.params.trial_ik_random.distance_current < 0.1) {
+      kineval.params.ik_target.position[0][0] = 1.2*(Math.random()-0.5);
+      kineval.params.ik_target.position[1][0] = 1.2*(Math.random()-0.5)+1.5;
+      kineval.params.ik_target.position[2][0] = 0.7*(Math.random()-0.5)+0.5;
+      kineval.params.trial_ik_random.targets += 1;
+      textbar.innerHTML = "IK trial Random: target " + kineval.params.trial_ik_random.targets + " reached at time " + kineval.params.trial_ik_random.time;
+  }
 }
+
 
 kineval.iterateIK = function iterate_inverse_kinematics(endeffector_target_world, endeffector_joint, endeffector_position_local) {
 
